5de20f673143c8f529fbbe2d5e12d1fa42b935c4
Fabio C. Barrionuevo da Luz <bnafta@gmail.com>
2014-10-22 20:34:28 -0300

    adicionado esqueleto de programa

---
+++ programa.py
@@ -0,0 +1,39 @@
+import sys
+
+
+def verifica_igual(a, b):
+    pass
+
+def soma(a, b)
+    return a + b
+
+def subtracao(a, b)
+    return a - b
+
+def faz_algo(faz=False, p=False):
+    """
+    >>> faz_algo(faz=True)
+    """
+    valor = 0
+    if faz:
+        if p:
+            print("estou em P")
+            return True
+        print("Estou em Faz")
+        valor = 1
+    print("passou no final")
+    return valor
+ 
+ 
+# if __name__ == "__main__": # pragma: no cover
+#     if len(sys.argv) > 1:
+#         faz_algo(faz=True, p=True)
+#     elif len(sys.argv) > 1:
+#         faz_algo(faz=True)
+#     else:
+#         faz_algo(faz=False)
+# 
+#     print(verifica_igual(2, 2))
+#     print(verifica_igual(2, 3))    
+     
+ 
